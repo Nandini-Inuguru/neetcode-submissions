c0caa26cb4b10da19ec81726aee5a4c6d139d137
Nandini-Inuguru <nandiniinuguru2006@gmail.com>
2026-07-16 09:52:56 +0530

    Add: regular-expression-matching - submission-0

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    vector<vector<int>> dp;
+
+    bool dfs(int i,int j,string &s,string &p){
+
+        if(j==p.size())
+            return i==s.size();
+
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        bool match =
+            (i<s.size()) &&
+            (s[i]==p[j] || p[j]=='.');
+
+        // Next character is '*'
+        if(j+1<p.size() && p[j+1]=='*'){
+
+            bool skip =
+                dfs(i,j+2,s,p);
+
+            bool take =
+                match && dfs(i+1,j,s,p);
+
+            return dp[i][j]=skip||take;
+        }
+
+        return dp[i][j]=
+            match &&
+            dfs(i+1,j+1,s,p);
+    }
+
+    bool isMatch(string s, string p) {
+
+        dp.assign(
+            s.size()+1,
+            vector<int>(p.size()+1,-1)
+        );
+
+        return dfs(0,0,s,p);
+    }
+};
